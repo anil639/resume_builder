63d42127790e17dc6480455dcd567c3bdd6e55bc
Anil kumar nayak <anilkumarnayak639@gmail.com>
2023-08-05 23:18:02 +0530

    Template added

--- src/Pages/ResumeTemplates.js
+++ src/Pages/ResumeTemplates.js
@@ -1,6 +1,25 @@
-import React from "react";
-import { Container, Typography, Box, Paper, Grid } from "@mui/material";
+import { React, useState } from "react";
+import {
+  Container,
+  Typography,
+  Grid,
+  Card,
+  CardMedia,
+  Button,
+  Collapse,
+} from "@mui/material";
+
 const ResumeTemplates = () => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
   return (
     <div>
       <Container>
@@ -10,6 +29,38 @@ const ResumeTemplates = () => {
         <Typography variant="h6" marginBottom={5}>
           Select a Template to get Started
         </Typography>
+        <Grid container spacing={5}>
+          <Grid item md={3}>
+            <Card
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+              sx={{
+                maxWidth: 345,
+                position: "relative",
+              }}
+            >
+              <CardMedia
+                component="img"
+                image="https://media.licdn.com/dms/image/C560BAQH9Cnv1weU07g/company-logo_200_200/0/1575479070098?e=2147483647&v=beta&t=i4Pp6zVfz5VAznPIik_ua4I75sKlu4yAdGKgHC9vpTo"
+              ></CardMedia>
+              {isHovered && (
+                <Button
+                  variant="contained"
+                  color="primary"
+                  style={{
+                    whiteSpace: "nowrap",
+                    position: "absolute",
+                    top: "50%",
+                    left: "50%",
+                    transform: "translate(-50%, -50%)",
+                  }}
+                >
+                  Use Template
+                </Button>
+              )}
+            </Card>
+          </Grid>
+        </Grid>
       </Container>
     </div>
   );
