3e962b5beb2f2769d000266bedda0f8b97eeba15
Anil kumar nayak <anilkumarnayak639@gmail.com>
2023-08-24 00:00:00 +0530

    updating Resume template and preview component.

--- src/Components/Preview/Preview.js
+++ src/Components/Preview/Preview.js
@@ -1,17 +1,92 @@
-import React from "react";
+import { React, useState, useEffect, useRef } from "react";
+import { Container, Grid, TextField, Button } from "@mui/material";
+import { useSelector } from "react-redux";
+import {
+  TEMPLATE_ONE,
+  TEMPLATE_TWO,
+  TEMPLATE_THREE,
+  TEMPLATE_FOUR,
+} from "../../Redux/Constant/ActionType";
 import TemplateOne from "../Templates/TemplateOne";
 import TemplateTwo from "../Templates/TemplateTwo";
 import TemplateThree from "../Templates/TemplateThree";
 import TemplateFour from "../Templates/TemplateFour";
+import jsPDF from "jspdf";
+import html2canvas from "html2canvas";
+import { useNavigate } from "react-router-dom";
 
 const Preview = () => {
+  const [templateName, setTemplateName] = useState("");
+  const resumePreviewRef = useRef(null);
+
+  useEffect(() => {
+    const savedTemplateName = localStorage.getItem("templateName");
+    if (savedTemplateName) {
+      setTemplateName(savedTemplateName);
+    }
+  }, []);
+
+  const handleTemplateName = (e) => {
+    const name = e.target.value;
+    setTemplateName(name);
+    // save template to local storage
+    localStorage.setItem("templateName", name);
+  };
+
+  // to save the PDF with the template name
+  const handleSavePdf = async () => {
+    const doc = new jsPDF();
+    const canvas = await html2canvas(resumePreviewRef.current); //Converting it to an image using html2canvas
+    const imageData = canvas.toDataURL("image/jpeg"); //change to formats
+    const pdfWidth = doc.internal.pageSize.getWidth();
+    const pdfHeight = (canvas.height * pdfWidth) / canvas.width;
+    doc.addImage(imageData, "JPEG", 0, 0, pdfWidth, pdfHeight);
+    doc.save(`${templateName}.pdf`);
+  };
+
+  const navigate = useNavigate();
+
+  let Template = useSelector((state) => state.Select_template);
+
   return (
     <div>
-      <h1>Preview page</h1>
-      <TemplateOne />
-      <TemplateTwo />
-      <TemplateThree />
-      <TemplateFour />
+      <Container>
+        <h1>Resume Preview</h1>
+        <Grid container>
+          <Grid xs={8}>
+            {Template == TEMPLATE_ONE && <TemplateOne />}
+            {Template == TEMPLATE_TWO && <TemplateTwo />}
+            {Template == TEMPLATE_THREE && <TemplateThree />}
+            {Template == TEMPLATE_FOUR && <TemplateFour />}
+          </Grid>
+
+          <Grid xs={4}>
+            <h1>Create File Name</h1>
+            <TextField
+              label="Template Name"
+              variant="outlined"
+              value={templateName}
+              onChange={handleTemplateName}
+            ></TextField>
+            <div style={{ marginTop: "15px" }}>
+              <Button
+                variant="outlined"
+                sx={{ marginRight: "15px" }}
+                onClick={() => navigate(-1)}
+              >
+                back
+              </Button>
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={handleSavePdf}
+              >
+                Save
+              </Button>
+            </div>
+          </Grid>
+        </Grid>
+      </Container>
     </div>
   );
 };
--- src/Pages/ResumeTemplates.js
+++ src/Pages/ResumeTemplates.js
@@ -6,20 +6,50 @@ import {
   Card,
   CardMedia,
   Button,
-  Collapse,
 } from "@mui/material";
+import TemplateOne_img from "../Images/TemplateOne_img.png";
+import TemplateTwo_img from "../Images/TemplateTwo_img.png";
+import TemplateThree_img from "../Images/TemplateThree_img.png";
+import TemplateFour_img from "../Images/TemplateFour_img.png";
 import { Link } from "react-router-dom";
+import { T1, T2, T3, T4 } from "../Redux/Actions/SelectTemplate";
+import { useDispatch } from "react-redux";
 
 const ResumeTemplates = () => {
+  //state for Hovering over the img
   const [isHovered, setIsHovered] = useState(false);
+  const [isHovered1, setIsHovered1] = useState(false);
+  const [isHovered2, setIsHovered2] = useState(false);
+  const [isHovered3, setIsHovered3] = useState(false);
 
+  //Mouse events
   const handleMouseEnter = () => {
     setIsHovered(true);
+  };
+  const handleMouseEnter1 = () => {
+    setIsHovered1(true);
+  };
+  const handleMouseEnter2 = () => {
+    setIsHovered2(true);
+  };
+  const handleMouseEnter3 = () => {
+    setIsHovered3(true);
   };
 
   const handleMouseLeave = () => {
     setIsHovered(false);
   };
+  const handleMouseLeave1 = () => {
+    setIsHovered1(false);
+  };
+  const handleMouseLeave2 = () => {
+    setIsHovered2(false);
+  };
+  const handleMouseLeave3 = () => {
+    setIsHovered3(false);
+  };
+
+  const dispatch = useDispatch();
 
   return (
     <div>
@@ -40,15 +70,114 @@ const ResumeTemplates = () => {
                 position: "relative",
               }}
             >
-              <CardMedia
-                component="img"
-                image="https://media.licdn.com/dms/image/C560BAQH9Cnv1weU07g/company-logo_200_200/0/1575479070098?e=2147483647&v=beta&t=i4Pp6zVfz5VAznPIik_ua4I75sKlu4yAdGKgHC9vpTo"
-              ></CardMedia>
+              <CardMedia component="img" image={TemplateOne_img}></CardMedia>
               {isHovered && (
                 <Link to="/DetailsFillng">
                   <Button
                     variant="contained"
                     color="primary"
+                    onClick={() => {
+                      dispatch(T1());
+                    }}
+                    style={{
+                      whiteSpace: "nowrap",
+                      position: "absolute",
+                      top: "50%",
+                      left: "50%",
+                      transform: "translate(-50%, -50%)",
+                    }}
+                  >
+                    Use Template
+                  </Button>
+                </Link>
+              )}
+            </Card>
+          </Grid>
+          <Grid item md={3}>
+            <Card
+              onMouseEnter={handleMouseEnter1}
+              onMouseLeave={handleMouseLeave1}
+              sx={{
+                maxWidth: 345,
+                height: "100%",
+                position: "relative",
+              }}
+            >
+              <CardMedia component="img" image={TemplateTwo_img}></CardMedia>
+              {isHovered1 && (
+                <Link to="/DetailsFillng">
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={() => {
+                      dispatch(T2());
+                    }}
+                    style={{
+                      whiteSpace: "nowrap",
+                      position: "absolute",
+                      top: "50%",
+                      left: "50%",
+                      transform: "translate(-50%, -50%)",
+                    }}
+                  >
+                    Use Template
+                  </Button>
+                </Link>
+              )}
+            </Card>
+          </Grid>
+          <Grid item md={3}>
+            <Card
+              onMouseEnter={handleMouseEnter2}
+              onMouseLeave={handleMouseLeave2}
+              sx={{
+                maxWidth: 345,
+                height: "100%",
+                position: "relative",
+              }}
+            >
+              <CardMedia component="img" image={TemplateThree_img}></CardMedia>
+              {isHovered2 && (
+                <Link to="/DetailsFillng">
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={() => {
+                      dispatch(T3());
+                    }}
+                    style={{
+                      whiteSpace: "nowrap",
+                      position: "absolute",
+                      top: "50%",
+                      left: "50%",
+                      transform: "translate(-50%, -50%)",
+                    }}
+                  >
+                    Use Template
+                  </Button>
+                </Link>
+              )}
+            </Card>
+          </Grid>
+          <Grid item md={3}>
+            <Card
+              onMouseEnter={handleMouseEnter3}
+              onMouseLeave={handleMouseLeave3}
+              sx={{
+                maxWidth: 345,
+                height: "100%",
+                position: "relative",
+              }}
+            >
+              <CardMedia component="img" image={TemplateFour_img}></CardMedia>
+              {isHovered3 && (
+                <Link to="/DetailsFillng">
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={() => {
+                      dispatch(T4());
+                    }}
                     style={{
                       whiteSpace: "nowrap",
                       position: "absolute",
